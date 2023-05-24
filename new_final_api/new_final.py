d42533bda16ea4a736e3416b5611aef71e8c061f
Akshit Gulyan <103456810+AkshitGulyan@users.noreply.github.com>
2023-05-24 07:45:40 +0530

    Update new_final.py

--- new_final_api/new_final.py
+++ new_final_api/new_final.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 import requests
 app=Flask(__name__)
  
-@app.route("/platform")
+@app.route("/user_id")
 def fetch_apps():
-    response=requests.get('http://127.0.0.1:5000/api/codechef/garvit_47')
+    response=requests.get('http://127.0.0.1:5000/api')
 
     if response.status_code == 200:
         apps =response.json()
