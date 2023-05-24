804919f6b7aedac02a857d7cbaac428034c909a1
Akshit Gulyan <103456810+AkshitGulyan@users.noreply.github.com>
2023-05-24 10:30:48 +0530

    Update main.py
    Changed main.py 
    ended the usage of new_final.py

--- new_final_api/main.py
+++ new_final_api/main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from flask import Flask, jsonify
+import requests
+from details_soup import UserData
 
-from new_final import fetch_apps
+# from new_final import fetch_apps
 # from details_soup import UserData
 
 
@@ -24,6 +27,15 @@ api = Api(app)
 
 
 # api.add_resource(Details,'/api/<string:platform>/<string:username>')
+@app.route("/username")
+def fetch_apps():
+    response=requests.get('http://127.0.0.1:5000/api/leetcode/dubey0613')
+
+    if response.status_code == 200:
+        apps =response.json()
+        return jsonify(apps)
+    else:
+        return jsonify({"Error": "Oops Sorry"})
 
 @app.errorhandler(404)
 def invalid_route(e):
